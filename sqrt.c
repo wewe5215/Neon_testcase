6bda64e0d6fb38691027c043460d0c212bb08b0e
Wewe <wewe5215@gmail.com>
2023-08-21 23:57:02 +0800

    fix:sqrt

--- sqrt.c
+++ sqrt.c
@@ -206,7 +206,7 @@ int main(){
 
 void f1(){
     for (size_t i = 0 ; i < (sizeof(test_vec_f32) / sizeof(test_vec_f32[0])) ; i++) {
-        float32x2_t a = vld1q_s32(test_vec_f32[i].a);
+        float32x2_t a = vld1_s32(test_vec_f32[i].a);
         float32x2_t rst = vsqrt_f32(a);
         printf("{ ");
         for(int j = 0;j < 2;j ++){
@@ -234,12 +234,7 @@ void f3(){
     for (size_t i = 0 ; i < (sizeof(test_vec_f64) / sizeof(test_vec_f64[0])) ; i++) {
         float64x1_t a = vld1_f64(test_vec_f64[i].a);
         float64x1_t rst = vsqrt_f64(a);
-        printf("{ ");
-        
-        test_codegen_f64(rst);
-            
-        
-        printf(" }\n");
+        print_int64x1(rst);
     }
 }
 
@@ -259,7 +254,7 @@ void f4(){
 void f5(){
     for (size_t i = 0 ; i < (sizeof(test_vec_f16h) / sizeof(test_vec_f16h[0])) ; i++) {
         // float16_t a = vld1_f16(test_vec_f32[i].a);
-        float16_t rst = vsqrth_f16(test_vec_f32[i].a);
+        float16_t rst = vsqrth_f16(test_vec_f16h[i].a);
         printf("{ ");
 
         test_codegen_f16(rst);
